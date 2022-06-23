271c58249b5bbbb9b6a533b40c09475ec3ec9b62
dumantaner <98847776+dumantaner@users.noreply.github.com>
2022-06-23 21:45:43 +0300

    Custom Pie Feature

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -11,6 +11,7 @@ export class ProductComponent implements OnInit {
   constructor() { }
   title="Ürün Listesi"
   buy="Satın Al"
+  filterText=""
   products: Product[] = [
   
     {id:1,name:"Laptop", price:2500,categoryId:1,description:"Asus Zenbook",imageUrl:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"},
