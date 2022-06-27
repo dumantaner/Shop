9cfc7e6fed4f3d737caf7b50b6c8bcc88c0fc1f6
emregoren <emre.goren@lunes.de>
2022-06-27 15:13:17 +0300

    http

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../product/product';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, pipe, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { AlertifyService } from './alertify.service';
 
@@ -15,11 +15,11 @@ export class ProductService {
   path = "http://localhost:3000/products";
 
   getProducts(categoryId: any): Observable<Product[]> {
-    
-   let newPath=this.path
-   if(categoryId){
-   newPath= newPath+"?categoryId=" + categoryId
-   }
+
+    let newPath = this.path
+    if (categoryId) {
+      newPath = newPath + "?categoryId=" + categoryId
+    }
 
 
     return this.http.get<Product[]>(newPath).pipe(
@@ -27,7 +27,20 @@ export class ProductService {
       catchError(this.handleError)
     );
   }
-
+  
+  addProduct(product:Product):Observable<Product> {
+    
+    const httpOptions ={
+      headers:new HttpHeaders({
+        'Content-type':'application/json',
+        'Authorization':'Token'
+      })
+    }
+    return this.http.post<Product>(this.path,product,httpOptions).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
   handleError(err: HttpErrorResponse) {
     let errorMessage = ''
     if (err.error instanceof ErrorEvent) {
