4b6ec42fe53bacfc65c1aa29c3c862f9aa07f1b2
dumantaner <98847776+dumantaner@users.noreply.github.com>
2022-06-29 15:23:29 +0300

    delete

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,35 +3,35 @@ import { Product } from './product';
 import { AlertifyService } from '../services/alertify.service';
 import { ProductService } from '../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
-  providers:[ProductService]
+  providers: [ProductService]
 })
 export class ProductComponent implements OnInit {
 
   constructor(private alertifyService: AlertifyService,
-    private productService:ProductService,
-    private activatedRoute:ActivatedRoute
-    ) { }  // yapıcı metod içerisine instance oluşturuluyor
-  
-    title = "Ürün Listesi"
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute, private http: HttpClient
+  ) { }  // yapıcı metod içerisine instance oluşturuluyor
+
+  title = "Ürün Listesi"
   buy = "Sepete Ekle"
+  delete = "Sil"
   filterText = ""
   products: Product[] = [];
- 
+  path = "http://localhost:3000/products";
 
   ngOnInit(): void {
 
-    this.activatedRoute.params.subscribe(params=>{
-      this.productService.getProducts(params["categoryId"]).subscribe(data=>{
-        this.products=data
+    this.activatedRoute.params.subscribe(params => {
+      this.productService.getProducts(params["categoryId"]).subscribe(data => {
+        this.products = data
+      })
     })
-
-
-})
 
 
   }
@@ -39,9 +39,20 @@ export class ProductComponent implements OnInit {
   addToCard(_product: Product) {
     this.alertifyService.set('notifier', 'position', 'top-right');
 
-    this.alertifyService.warning(_product.name + "  Sepete Eklendi");
+    this.alertifyService.success(_product.name + "  Sepete Eklendi");
 
 
   }
+
+
+  deleteToCard(_product: Product) {
+    this.alertifyService.warning(_product.name + "  Silindi");
+    let newPath = this.path;
+    newPath = newPath + '/' + _product.id
+    this.http.delete(newPath).subscribe();
+    
+  }
+
+
 
 }
